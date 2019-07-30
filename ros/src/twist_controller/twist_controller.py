a6310395fd68a10d5cd044bd446c6b2a5927b20d
Joachim Rissler <33800285+JR-Git0@users.noreply.github.com>
2019-07-30 18:07:56 +0200

    Update brake value
    Keeps car in place, when stopped at a traffic light

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -50,7 +50,7 @@ class Controller(object):
 
         if linear_vel==0 and current_vel<1.5 :
             throttle=0
-            brake=400
+            brake=700 # N*m - to hold the car in place if we are stoped at a light - Acceleration - lm/s^2
         elif throttle<0.1 and vel_error<0 :
             throttle=0
             decel=max(vel_error, self.decel_limit)
